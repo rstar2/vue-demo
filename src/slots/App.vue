9937bfeac9fcc9a3118134152e01b40696a55f1c
rstar2 <rstar2@abv.bg>
2018-06-04 12:21:12 +0300

    ad slots

---
+++ src/slots/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <BaseLayout>
+    <!-- If used <template> then the corresponding slot in the patent is replaced with the contents-->
+    <template slot="header">
+      <h1>Here might be a page title</h1>
+    </template>
+
+    <!-- This is the dafault slot,
+      e.g. same as <template slot="default"></template>
+      or just <template slot=></template> -->
+    <p>A paragraph for the main content.</p>
+    <p>And another one.</p>
+
+    <!-- Could be used any HDML tag, not just <template> -->
+    <!-- IN thi scase the corresponding slot in the parent is replaced with the whole tag (DIV in this case) -->
+    <div slot="footer">
+      <p>Here's some contact info</p>
+    </div>
+
+    <!-- Extract the passed prom the parent BaseLayout component scope properties. Here we use ES6 destructuring  -->
+    <div slot="footer-ex" slot-scope="{result, choices}">
+      <p v-if="!result.isFinished">Result: {{result.value}}</p>
+      <p v-for="(choice, index) in choices" :key="index">{{choice}}</p>
+    </div>
+  </BaseLayout>
+</template>
+
+<script>
+import BaseLayout from './BaseLayout.vue';
+
+export default {
+  components: {
+    BaseLayout
+  }
+};
+</script>
---
+++ src/slots/BaseLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <header>
+      <div>Header</div>
+      <slot name="header">Default Header content - if 'header' slot is not provided</slot>
+      <hr>
+    </header>
+
+    <main>
+      <div>Main</div>
+      <slot>Default Main content - if 'default' slot is not provided</slot>
+      <hr>
+    </main>
+
+    <footer>
+      <div>Footer</div>
+      <slot name="footer">Default Footer content - if 'footer' slot is not provided</slot>
+    </footer>
+
+    <!-- Pass scoped props to the scoped child template -->
+    <slot name="footer-ex" :result="result" :choices="data" >Ups</slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      result: {value: 1, finished: false},
+      data: ["One", "Two", "Three"]
+    }
+  }
+};
+</script>
