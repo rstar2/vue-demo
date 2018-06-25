bedc99cc2dc046222f409a09304448139be0de2e
Rumen Neshev <rstar2@abv.bg>
2018-06-26 00:24:59 +0300

    RxJS liniar interpolation animation

--- src/rxjs/App.vue
+++ src/rxjs/App.vue
@@ -1,66 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h2>Slides:</h2>
-    <h2>{{ slide }}</h2>
-    <button @click="onSlide(-1)">Prev</button>
-    <button @click="onSlide(1)">Next</button>
+    <Slides/>
+    <CardAnimate/>
   </div>
 </template>
 
 <script>
-import * as RxJS from "rxjs";
+import Slides from "./Slides.vue";
+import CardAnimate from "./CardAnimate.vue";
 
 export default {
-  data() {
-    return {
-      slide: "Test"
-    };
-  },
-  methods: {
-    onSlide(count) {
-      this.slideButtons$.next(count);
-    }
-  },
-  mounted() {
-    const slides = ["0", "1", "2", "3", "4"];
-
-    // // 'zip' with 'interval' makes scheduled emitting from a collection
-    // let slides$ = RxJS.Observable.zip(
-    //   RxJS.Observable.from(slides),
-    //   RxJS.Observable.interval(1000).take(slides.length),
-    // )
-    // .repeat(-1) // 'repeat' indefinately
-    // .subscribe(
-    //   ([slide]) => {
-    //     this.slide = slide;
-    //   }
-    // );
-
-    // this stream will listen to the buttons controlling the slider
-    this.slideButtons$ = new RxJS.Subject();
-
-    const keys$ = RxJS.Observable.fromEvent(document, "keydown").pluck("key");
-    const left$ = keys$.filter(key => key === "ArrowLeft").mapTo(-1);
-    const right$ = keys$.filter(key => key === "ArrowRight").mapTo(1);
-
-    // this stream will listen to the keys controlling the slider
-    const slideKeys$ = RxJS.Observable.merge(left$, right$).throttleTime(250);
-
-    // this is converted stream (from both slider controlling streams) to slide index
-    const index$ = RxJS.Observable.merge(this.slideButtons$, slideKeys$)
-      .startWith(0)
-      .scan((index, count) => index + count);
-
-    // extract the real slide from the
-    const slides$ = RxJS.Observable.of(slides)
-      .combineLatest(index$)
-      .map(([slides, index]) => [slides, index % slides.length])
-      .map(([slides, index]) => [slides, index >= 0 ? index : slides.length + index])
-      .map(([slides, index]) => slides[index])
-      .subscribe(slide => {
-        this.slide = slide;
-      });
+  components: {
+    Slides,
+    CardAnimate
   }
 };
 </script>
