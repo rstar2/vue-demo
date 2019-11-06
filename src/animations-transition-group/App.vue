87633d32d5f96bfe84ca357127e286cd072a9eb4
Rumen Neshev <rstar2@abv.bg>
2019-11-06 22:58:35 +0200

    asd

---
+++ src/animations-transition-group/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Items />
+  </div>
+</template>
+
+<script>
+import Items from "./components/Items";
+
+export default {
+  components: {
+    Items
+  },
+};
+</script>
