146ef62d1d80067278001ecdb0673f9fde1db77d
Rumen Neshev <rstar2@abv.bg>
2019-12-02 23:22:31 +0200

    asd

---
+++ src/animation-gsap/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- <Spinners /> -->
+  </div>
+</template>
+
+<script>
+// https://blog.logrocket.com/animating-vue-with-greensock/
+// import Spinners from "./components/Spinners.vue";
+
+export default {
+  components: {
+    // Spinners
+  }
+};
+</script>
